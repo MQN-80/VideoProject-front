f9a937b417362c1bdf74c82c386dc6afa6ab7294
LAPTOP-UOABPOLV\ASUS <1060696988@qq.com>
2022-12-01 22:15:18 +0800

    123

--- run-tj/app/src/main/java/activity/UserCenterActivity.java
+++ run-tj/app/src/main/java/activity/UserCenterActivity.java
@@ -37,7 +37,6 @@ public class UserCenterActivity extends AppCompatActivity {
 
         // 启动
 
-        //这一行有错，不知道有啥用
         NavigationUI.setupActionBarWithNavController(this,navController, appBarConfiguration);
 
         NavigationUI.setupWithNavController(navView, navController);
